2ddbd06f8da5ae904016a1348e3a866e9fba89cb
Hannes Schaller <admin@cyberkov.at>
2026-02-02 21:56:28 +0000

    commit what i have for now

--- cities.js
+++ cities.js
@@ -1,7 +1,437 @@
 CITIES =
 [
-   {name: 'Venice', latitude: 45.4408, longitude: 12.3155, radius: 3, fillKey: 'city', date: '1963-10'},
-   // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
-   {name: 'Khao Phing Kan', latitude: 8.2745, longitude: 98.5012, radius: 3, fillKey: 'city', date: '1974-12'},
-   {name: 'San Francisco', latitude: 37.7749, longitude: -122.4194, radius: 3, fillKey: 'city', date: '1985-05'},
+  {name: 'Venice', latitude: 45.4408, longitude: 12.3155, radius: 3, fillKey: 'city'},
+  {
+    "name": "Los Angeles",
+    "latitude": 34.0522342,
+    "longitude": -118.2436849,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {
+    "name": "Hawaii",
+    "latitude": 19.5429151,
+    "longitude": -155.66585680000003,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Wien",
+    "latitude": 48.2081743,
+    "longitude": 16.37381890000006,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Sankt Pölten",
+    "latitude": 48.20353,
+    "longitude": 15.638169999999946,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Linz",
+    "latitude": 48.30694,
+    "longitude": 14.285830000000033,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Salzburg",
+    "latitude": 47.80949,
+    "longitude": 13.055010000000038,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Ried im Innkreis",
+    "latitude": 48.21272,
+    "longitude": 13.492719999999963,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Innsbruck",
+    "latitude": 47.2692124,
+    "longitude": 11.404102400000056,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Bregenz",
+    "latitude": 47.50075,
+    "longitude": 9.742309999999975,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Feldkirch",
+    "latitude": 47.24128,
+    "longitude": 9.6019,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Graz",
+    "latitude": 47.070714,
+    "longitude": 15.439503999999943,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2003-06-22"
+  },
+  {
+    "name": "Klagenfurt",
+    "latitude": 46.6364598,
+    "longitude": 14.312224600000036,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Kernhof",
+    "latitude": 47.81592999999999,
+    "longitude": 15.544200000000046,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2012-05-13"
+  },
+  {
+    "name": "Tulln an der Donau",
+    "latitude": 48.3314949,
+    "longitude": 16.060737000000017,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2013-06-30"
+  },
+  {
+    "name": "Bad Mitterndorf",
+    "latitude": 47.55463,
+    "longitude": 13.936008300000026,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2013-08-11"
+  },
+  {
+    "name": "Gmunden",
+    "latitude": 47.91839,
+    "longitude": 13.799330000000054,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2013-08-23"
+  },
+  {
+    "name": "Dürnstein",
+    "latitude": 48.3976064,
+    "longitude": 15.51973810000004,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2010-05-24"
+  },
+  {
+    "name": "Dachstein",
+    "latitude": 47.4685545,
+    "longitude": 13.626002999999969,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2009-11-29"
+  },
+  {
+    "name": "Schladming",
+    "latitude": 47.38699152538012,
+    "longitude": 13.69769275188446,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Auckland",
+    "latitude": -37.0082476,
+    "longitude": 174.78503580000006,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {
+    "name": "Suva",
+    "latitude": -17.713371,
+    "longitude": 178.06503199999997,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Aitutaki",
+    "latitude": -18.8579523,
+    "longitude": -159.7852843,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Rarotonga",
+    "latitude": -21.2292371,
+    "longitude": -159.7763491,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Tonga",
+    "latitude": -21.178986,
+    "longitude": -175.198242,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Vavaʻu",
+    "latitude": -18.65,
+    "longitude": -173.98333300000002,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Birmingham",
+    "latitude": 52.48624299999999,
+    "longitude": -1.8904009999999971,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "London",
+    "latitude": 51.5073509,
+    "longitude": -0.12775829999998223,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2011-11-06"
+  },
+  {
+    "name": "Cambridge",
+    "latitude": 52.205337,
+    "longitude": 0.12181699999996454,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2011-03-18"
+  },
+  {
+    "name": "Oxford",
+    "latitude": 51.7520209,
+    "longitude": -1.2577263000000585,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2010-10-23"
+  },
+  {
+    "name": "Edinburgh",
+    "latitude": 55.953252,
+    "longitude": -3.188266999999996,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Aberdeen",
+    "latitude": 57.149717,
+    "longitude": -2.094278000000031,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Brighton",
+    "latitude": 50.82253000000001,
+    "longitude": -0.13716299999998682,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2012-12-11"
+  },
+  {
+    "name": "Dundee",
+    "latitude": 56.462018,
+    "longitude": -2.970721000000026,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Glasgow",
+    "latitude": 55.864237,
+    "longitude": -4.251805999999988,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Dublin",
+    "latitude": 53.3498053,
+    "longitude": -6.260309699999993,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Chania",
+    "latitude": 35.5138298,
+    "longitude": 24.01803670000004,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2009-06-24"
+  },
+  {
+    "name": "Berlin",
+    "latitude": 52.52000659999999,
+    "longitude": 13.404953999999975,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Leipzig",
+    "latitude": 51.3396955,
+    "longitude": 12.373074699999961,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Paris",
+    "latitude": 48.856614,
+    "longitude": 2.3522219000000177,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2011-04-13"
+  },
+  {
+    "name": "Venedig",
+    "latitude": 45.4408474,
+    "longitude": 12.31551509999997,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Madeira",
+    "latitude": 32.76070740000001,
+    "longitude": -16.959472300000016,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2011-08-03"
+  },
+  {
+    "name": "Lissabon",
+    "latitude": 38.7222524,
+    "longitude": -9.139336599999979,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Porto",
+    "latitude": 41.1579438,
+    "longitude": -8.629105299999992,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Cabo da Roca",
+    "latitude": 38.7802778,
+    "longitude": -9.498888899999997,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Santorini",
+    "latitude": 36.3931562,
+    "longitude": 25.461509200000023,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Nikosia",
+    "latitude": 35.166667,
+    "longitude": 33.36666700000001,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Ljubljana",
+    "latitude": 46.0569465,
+    "longitude": 14.505751499999974,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Straßburg",
+    "latitude": 48.5734053,
+    "longitude": 7.752111300000024,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Athen",
+    "latitude": 37.983917,
+    "longitude": 23.729359899999963,
+    "radius": 3,
+    "fillKey": "city"
+  },
+  {
+    "name": "Bratislava",
+    "latitude": 48.1458923,
+    "longitude": 17.107137299999977,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "2012-11-14"
+  },
+  {
+    "name": "Orléans",
+    "latitude": 47.902964,
+    "longitude": 1.9092510000000402,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "1990-06-03"
+  },
+  {
+    "name": "Blois",
+    "latitude": 47.58609209999999,
+    "longitude": 1.3359474999999748,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "1990-06-03"
+  },
+  {
+    "name": "Amboise",
+    "latitude": 47.413326,
+    "longitude": 0.9844070000000329,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "1990-06-03"
+  },
+  {
+    "name": "Tours",
+    "latitude": 47.394144,
+    "longitude": 0.6848400000000083,
+    "radius": 3,
+    "fillKey": "city",
+    "date": "1990-06-03"
+  },
+  {
+    "name": "Zürich",
+    "latitude": 47.3686498,
+    "longitude": 8.539182500000038,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {
+    "name": "Flughafen Amsterdam Schiphol",
+    "latitude": 52.3076865,
+    "longitude": 4.767424099999971,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {
+    "name": "München",
+    "latitude": 48.1351253,
+    "longitude": 11.581980599999952,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {
+    "name": "Düsseldorf",
+    "latitude": 51.2277411,
+    "longitude": 6.773455600000034,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {
+    "name": "Frankfurt",
+    "latitude": 50.1109221,
+    "longitude": 8.682126700000026,
+    "radius": 3,
+    "fillKey": "transit"
+  },
+  {name: 'Santa Cruz de La Palma', latitude: 28.6870233, longitude: -17.7815274, radius: 3, fillKey: 'city', date: "2019-06-03"},
 ]
--- countries.js
+++ countries.js
@@ -1,7 +1,21 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
+    AUT: {fillKey: 'visited'},
+    COK: {fillKey: 'visited'},
+    CYP: {fillKey: 'visited'},
+    CZE: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    FJI: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    GRC: {fillKey: 'visited'},
+    HUN: {fillKey: 'visited'},
     ITA: {fillKey: 'visited'},
-    USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
+    IRL: {fillKey: 'visited'},
+    PRT: {fillKey: 'visited'},
+    SVK: {fillKey: 'visited'},
+    SVN: {fillKey: 'visited'},
+    TON: {fillKey: 'visited'},
 }
